b26c2215ab842753512237c5a33223a5c1ab815c
Alex Song <song_liping@hotmail.com>
2018-03-15 17:12:27 +0800

    implement XML unmarsheller

---
+++ webcrawler/src/main/java/alex/beta/webcrawler/configuration/IConfiguration.java
@@ -0,0 +1,31 @@
+/**
+ * @File: IConfiguration.java
+ * @Project: beta
+ * @Copyright: Copyright (c) 2018, All Rights Reserved
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * </p>
+ * @Date: 2018/3/15 8:20
+ * @author: <a target=_blank href="mailto:song_liping@hotmail.com">Alex Song</a>
+ */
+package alex.beta.webcrawler.configuration;
+
+import java.util.List;
+
+/**
+ * @version ${project.version}
+ * @Description
+ */
+public interface IConfiguration {
+    List<String> getEntryPoints();
+
+    int getDepth();
+
+    int getNumberOfCrawlers();
+
+    String getClawerClass();
+
+    String getCrawlStorageFolder();
+}
