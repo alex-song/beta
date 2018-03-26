95bc8b0c5aa664b9e430a3de6c9993b50f2daf74
alex-song <song_liping@hotmail.com>
2018-03-26 21:40:49 +0800

    update docker file for postgres

--- postgres/src/main/docker/docker-entrypoint.sh
+++ postgres/src/main/docker/docker-entrypoint.sh
@@ -89,6 +89,7 @@ if [ "$1" = 'postgres' ]; then
 			echo
 			echo "host all all all $authMethod"
 			echo "host all all 0.0.0.0/0 md5"
+			echo "host all all 127.0.0.1/32 md5"
 		} >> "$PGDATA/pg_hba.conf"
 
 		# internal start of server in order to allow set-up using psql-client
