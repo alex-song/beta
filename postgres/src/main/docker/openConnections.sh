1f1a34df579f234e5de34d0c8de4da88cf002d6f
Alex Song <alex.song@hpe.com>
2018-03-27 16:35:37 +0800

    customize postgres docker image, to allow connections from anywhere

--- postgres/src/main/docker/openConnections.sh
+++ postgres/src/main/docker/openConnections.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-{
-	echo
-	echo "host    all    all        0.0.0.0/0     md5"
-	echo "host    all    all        0.0.0.0/0     password"
-	echo "host    all    all        ::0/0         md5"
-	echo "host    all    all        ::0/0         password"
-} >> "$PGDATA/pg_hba.conf"
+#{
+#	echo
+#	echo "host    all    all        0.0.0.0/0     md5"
+#	echo "host    all    all        0.0.0.0/0     password"
+#	echo "host    all    all        ::0/0         md5"
+#	echo "host    all    all        ::0/0         password"
+#} >> "$PGDATA/pg_hba.conf"
+
+mv -f $PGDATA/pg_hba.conf $PGDATA/pg_hba_org.conf
+cp pg_hba.conf $PGDATA/pg_hba.conf
