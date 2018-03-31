dc7d0290164e3780640f652753cafd674cddae0f
alex-song <song_liping@hotmail.com>
2018-03-31 16:15:22 +0800

    update mongo docker-compose script

--- filerepository/src/main/docker/mongo/mongo-entrypoint/inituser.js
+++ filerepository/src/main/docker/mongo/mongo-entrypoint/inituser.js
@@ -1,9 +1,26 @@
-var db = connect("mongodb://localhost/admin");
-
+// MongoDB initialization script for file repository project
+// Create super user in admin database
+var db = connect("admin");
 db.createUser(
     {
-        user: "alexsong",
-        pwd: "Asd123",
-        roles:['root']
+        user:"alexsong",
+        pwd:"Asd123",
+        roles:[{role: "root", db: "admin"}]
     }
-)
+);
+
+// Create frs-grid database, that persists large files using grid fs
+db = db.getSiblingDB('frs-grid');
+// Create frs database
+db = db.getSiblingDB('frs');
+
+// Create app user, and grant dbOwner role in both frs and frs-grid databases
+db.createUser(
+    {
+        user:"frs",
+        pwd:"frs",
+        roles:[{role:"dbOwner", db:"frs"}, {role:"dbOwner", db:"frs-grid"}]
+    }
+);
+
+
