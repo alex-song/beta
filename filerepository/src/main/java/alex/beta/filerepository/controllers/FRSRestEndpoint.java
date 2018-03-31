8989a95ce08080851fc00bbd411801e103cd9d7e
Alex Song <song_liping@hotmail.com>
2018-04-01 00:21:45 +0800

    lombok integrate with mongo

--- filerepository/src/main/java/alex/beta/filerepository/controllers/FRSRestEndpoint.java
+++ filerepository/src/main/java/alex/beta/filerepository/controllers/FRSRestEndpoint.java
@@ -12,14 +12,19 @@
  */
 package alex.beta.filerepository.controllers;
 
-import io.swagger.annotations.Api;
+import alex.beta.filerepository.persistence.entity.FileInfo;
+import alex.beta.filerepository.persistence.repository.FileInfoRepository;
+import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
@@ -38,13 +43,27 @@ public class FRSRestEndpoint {
 
     private MessageSource messageSource;
 
+    private FileInfoRepository frsRepository;
+
     @Autowired
-    public FRSRestEndpoint(MessageSource messageSource) {
+    public FRSRestEndpoint(MessageSource messageSource, FileInfoRepository frsRepository) {
         this.messageSource = messageSource;
+        this.frsRepository = frsRepository;
     }
 
     @GetMapping("/")
     void home(HttpServletResponse response) throws IOException {
         response.sendRedirect("api-spec/index.html");
+    }
+
+    @ApiOperation(value = "Test")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Translation request is found."),
+            @ApiResponse(code = 404, message = "Translation request is not found.")
+    })
+    @GetMapping(value = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity translate() {
+        frsRepository.save(FileInfo.builder().id(1L).name("name").description("description").build());
+        return ResponseEntity.ok("ok");
     }
 }
