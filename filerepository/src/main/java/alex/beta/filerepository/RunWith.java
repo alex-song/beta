5cc5eb2e2e4e060fd1cabadd26653fddef3ff45c
Alex Song <alex.song@hpe.com>
2018-04-20 11:40:00 +0800

    code refactor

--- filerepository/src/main/java/alex/beta/filerepository/RunWith.java
+++ filerepository/src/main/java/alex/beta/filerepository/RunWith.java
@@ -20,9 +20,9 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static alex.beta.filerepository.SecurityConfig.*;
@@ -91,7 +91,7 @@ public class RunWith {
         }
         final Authentication originalAuthentication = SecurityContextHolder.getContext() == null ? null : SecurityContextHolder.getContext().getAuthentication();
 
-        List<GrantedAuthority> authorities = new ArrayList<>();
+        Set<GrantedAuthority> authorities = new LinkedHashSet<>();
         if (roles != null) {
             Arrays.stream(roles).forEach(role -> authorities.add(new SimpleGrantedAuthority(role)));
         }
